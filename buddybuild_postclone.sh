be21ffdf1e78fce17fc39bc5f2ac4506553cb65e
Romain Pouclet <romain.pouclet@gmail.com>
2019-09-24 08:30:53 -0700

    Resolve deps explicitly

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -5,3 +5,6 @@ echo "Changing SCM settings..."
 defaults write com.apple.dt.xcodebuild IDEPackageSupportUseBuiltinSCM -bool YES
 defaults write com.apple.dt.Xcode IDEPackageSupportUseBuiltinSCM -bool YES
 
+echo "Resolving dependencies..."
+
+xcodebuild -resolvePackageDependencies
